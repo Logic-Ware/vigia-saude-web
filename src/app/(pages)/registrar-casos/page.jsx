a63f4238c9eb97dd3ae56c8f0822ac9dbbad5733
adrian-siv <adrian.ssiv@gmail.com>
2023-11-24 20:45:10 -0300

    change to unique style

---
+++ src/app/(pages)/registrar-casos/page.jsx
@@ -0,0 +1,12 @@
+export default function RegistrarCasos() {
+    return (
+        <>
+        <main>
+            <section>
+                <h1>Proposta desta página</h1>
+                <p>Segundo nossa ideia de projeto, que infelizmente não conseguimos implementar completamente em tempo hábil, esta seria a página onde médicos e administradores/sistemas automatizados de hospitais iriam dar entrada em novos de doenças (ex.: Tuberculose).</p>
+            </section>
+        </main>
+        </>
+    )
+}
