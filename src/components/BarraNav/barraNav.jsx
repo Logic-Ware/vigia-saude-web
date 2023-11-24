bc3d7c4a395f586db9c44c727a4925760a0f750f
adrian-siv <adrian.ssiv@gmail.com>
2023-11-24 19:46:40 -0300

    add arrowBack

--- src/components/BarraNav/barraNav.jsx
+++ src/components/BarraNav/barraNav.jsx
@@ -1,21 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 import logoVigiaSaude from "/public/logo_vigia_saude.png";
-import "./barraNav.scss"
+import "./barraNav.scss";
+import { IoArrowBack } from 'react-icons/io5';
+
 
 export default function BarraNav() {
-    return (
-        <>
-        <nav>
-            <Link href="">
-                <Image
-                width={50}
-                height={50}
-                src={logoVigiaSaude}
-                alt="Logo do Vigia Saúde"
-                />
-            </Link>
-        </nav>
-        </>
-    )
+	return (
+		<>
+			<nav>
+				<Link href={"/"}>
+					<IoArrowBack />
+				</Link>
+				<Link href="">
+					<Image
+						width={50}
+						height={50}
+						src={logoVigiaSaude}
+						alt="Logo do Vigia Saúde"
+					/>
+				</Link>
+			</nav>
+		</>
+	);
 }
