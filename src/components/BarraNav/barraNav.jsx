98b4a5680e1414aab440b0ededc31c811f0b4ca5
jaciteixeira <jaci9306@gmail.com>
2023-11-24 21:01:46 -0300

    adicionado link "sair"

--- src/components/BarraNav/barraNav.jsx
+++ src/components/BarraNav/barraNav.jsx
@@ -1,9 +1,30 @@
+'use client'
 import Link from "next/link";
 import Image from "next/image";
 import logoVigiaSaude from "/public/logo_vigia_saude.png";
 import "./barraNav.scss"
+import { useEffect, useRef, useState } from 'react'
 
 export default function BarraNav() {
+    
+        /**
+     * Variável que guarda a referencia do div que contém os links
+     * @type {React.MutableRefObject<HTMLDivElement>}
+     */
+        const linksDiv = useRef(null)
+
+    /**
+     * Variável que guarda a referencia do div que contém o hamburger
+     * @type {React.MutableRefObject<HTMLDivElement>}
+     */
+    
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setUser(localStorage.getItem('user'))
+        }
+    }, [])
     return (
         <>
         <nav>
@@ -30,7 +51,20 @@ export default function BarraNav() {
                     <Link href={'/cadastro'}>Cadastrar</Link>
                 </li>
                 <li class="links">
-                    <Link href={'/login'}>Entrar</Link>
+                {!user ? (
+                <Link href="/login">Entrar</Link>
+              ) : (
+                <Link
+                  onClick={(e) => {
+                    e.preventDefault();
+                    localStorage.removeItem('user');
+                    setUser(null);
+                  }}
+                  href={'#'}
+                >
+                  Sair
+                </Link>
+              )}
                 </li>
                 
             </ul>
