b0f2e7540c2f4e0d67c755b1391a34d820c44a80
PedroPaterno <rm99726@fiap.com.br>
2023-11-24 22:42:08 -0300

    estilizado o home

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -1,8 +1,25 @@
+import Image from "next/image";
 import Link from "next/link";
+import "./page.scss";
+import imagemAzul from "/public/imagem-azul.png";
 
 export default function Home() {
   return <main>
-    <Link href={'/cadastro'}>Cadastrar</Link>
-    <Link href={'/login'}>Entrar</Link>
+    <div class="text">
+      <p>
+         O objetivo central do nosso projeto é implementar um sistema de monitoramento baseado em inteligência artificial para detectar padrões anormais de incidência de doenças. Essa abordagem possibilitaria uma resposta mais rápida a possíveis surtos, contribuindo significativamente para o controle de epidemias.
+      </p> 
+      <p>
+         Idealmente, buscamos integrar uma API de coleta de dados aos sistemas de hospitais, tanto públicos quanto privados. Essa API receberia em tempo real a entrada de incidência de casos de doenças, registrando as informações em bancos de dados. Os dados coletados, a princípio, incluiriam a doença, o local (estado) e a idade do paciente.
+      </p>
+    </div>
+
+      <Image class="imagem"
+        width={500}
+        height={500}
+        src={imagemAzul}
+        alt="Imagem-azul"
+      />
+
   </main>;
 }
